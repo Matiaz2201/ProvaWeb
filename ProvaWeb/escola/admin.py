81e039b0bebe86159bccef62afab304fb1614314
Matiaz2201 <abnermatias2201@outlook.com>
2019-11-29 21:29:40 -0300

    final
    final

--- ProvaWeb/escola/admin.py
+++ ProvaWeb/escola/admin.py
@@ -1,29 +1,26 @@
 from django.contrib import admin
-from .models import Aluno, Disciplina, Professor, Matricula, Alocacao
+from .models import Alunos, Disciplinas, Professores, Matriculas, Alocacao
 
 # Register your models here.
 
-@admin.register(Aluno)
-class lista_aluno(admin.ModelAdmin):
+@admin.register(Alunos)
+class Alunos(admin.ModelAdmin):
     list_display = ['id', 'nome']
 
 
-@admin.register(Disciplina)
-class lista_disciplina(admin.ModelAdmin):
-    list_display = ['id', 'titulo_dis']
+@admin.register(Disciplinas)
+class Disciplinas(admin.ModelAdmin):
+    list_display = ['id', 'disciplina']
 
 
-@admin.register(Professor)
-class lista_professor(admin.ModelAdmin):
-    list_display = ['id', 'nome_prof', 'end', 'titulacao', 'fone']
+@admin.register(Professores)
+class Professores(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'endereco', 'titulacao', 'fone']
 
-@admin.register(Matricula)
-class lista_disciplina(admin.ModelAdmin):
-    list_display = ['id', 'nome_aluno', 'titulo_disciplina', 'faltas', 'ano', 'nota_final']
+@admin.register(Matriculas)
+class Matriculas(admin.ModelAdmin):
+    list_display = ['id', 'aluno', 'disciplina', 'faltas', 'ano', 'nota_final']
 
 @admin.register(Alocacao)
-class lista_disciplina(admin.ModelAdmin):
-    list_display = ['id', 'nome_prof', 'titulo_disc', 'horario', 'ano_letivo', 'carga']
-
-
-
+class Alocacao(admin.ModelAdmin):
+    list_display = ['id', 'professor', 'disciplina', 'horario', 'ano_letivo', 'carga']
--- ProvaWeb/escola/models.py
+++ ProvaWeb/escola/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Aluno(models.Model):
+class Alunos(models.Model):
     nome = models.CharField(max_length=40)
     idade = models.IntegerField()
 
@@ -11,41 +11,41 @@ class Aluno(models.Model):
         return self.nome
 
 
-class Disciplina(models.Model):
-    titulo_dis = models.CharField(max_length=50)
+class Disciplinas(models.Model):
+    disciplina = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.titulo_dis
+        return self.disciplina
 
 
-class Professor(models.Model):
-    nome_prof = models.CharField(max_length=40)
-    end = models.CharField(max_length=100)
+class Professores(models.Model):
+    nome = models.CharField(max_length=40)
+    endereco = models.CharField(max_length=100)
     titulacao = models.CharField(max_length=52)
     fone = models.CharField(max_length=12)
 
     def __str__(self):
-        return self.nome_prof
+        return self.nome
 
-class Matricula(models.Model):
-    nome_aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
-    titulo_disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+class Matriculas(models.Model):
+    aluno = models.ForeignKey(Alunos, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplinas, on_delete=models.CASCADE)
     ano = models.IntegerField()
     faltas = models.IntegerField()
     nota_final = models.FloatField()
 
     def __str__(self):
-        return self.nome_aluno.nome
+        return self.aluno.nome
 
 
 class Alocacao(models.Model):
-    nome_prof = models.ForeignKey(Professor, on_delete=models.CASCADE)
-    titulo_disc = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professores, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplinas, on_delete=models.CASCADE)
     horario = models.DateTimeField()
     ano_letivo = models.DateField()
     carga = models.IntegerField()
 
 
     def __str__(self):
-        retorno = self.nome_prof.nome_prof + ',' + self.titulo_disc.titulo_dis
+        retorno = self.professor.nome + ',' + self.disciplina.disciplina
         return retorno
